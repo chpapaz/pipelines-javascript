e4e53a447c0301978de7b5b51a58e6412e721a7f
chpapaz <59761849+chpapaz@users.noreply.github.com>
2020-01-11 13:08:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Node.js Sample123!\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time is currently: " + dt.myDateTime());
+  res.end();
 });
 
 var port = process.env.PORT||PORT;
